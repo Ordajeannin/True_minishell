731fb9e64a8de20f3e211de9a7fe37471ce5b2e0
ajeannin <ajeannin@student.42.fr>
2023-09-05 15:46:08 +0200

    ADD script for tests
    Script utilisable et ergonomique, a completer au fur et a mesure. Lance
    une batterie de tests. A utiliser a chaque modifiction de fichier, pour
    s'assurer que rien n'est casse.
    
    Norme OK
    Compilation OK

---
+++ script.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+################################################################################################
+#TESTS
+# Faire attention a correctement remplir les deux tableaux suivants.
+# Garder une logique dans l'ordre des tests.
+# Commenter le but des tests + erreurs potentielles
+
+# Definition des tableaux commandes / sorties attendues
+commands=(
+    "ls"
+	"pwd"
+	"cat makefile"
+	"echo Hello, world!" # w/o quote, multiples arg
+    "echo 'Hello, world!'" # single quotes, single arg
+	"echo \"Hello, world!\"" # double quotes, single arg
+	"echo 'Hello, \"world!\"' I love u" # mix args and quotes
+#	"echo Hello\"World\"" # str"str" bug connu, test de sensibilite du script
+)
+
+expected_output=(
+	"$(ls)"
+	"$(pwd)"
+	"$(cat makefile)"
+	"$(echo Hello, world!)"
+	"$(echo 'Hello, world!')"
+	"$(echo \"Hello, world!\")"
+	"$(echo 'Hello, "world!"' I love u)"
+#	"$(echo Hello"World")"
+)
+
+
+
+################################################################################################
+#SCRIPT
+
+# Definition des couleurs (codes d'echappement ANSI)
+WHITE='\033[0;37m'
+CYAN='\033[0;36m'
+MAGENTA='\033[0;35m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # Reinitialisation de la couleur a la normale
+
+# Verification de la longueur des tableaux
+if [ "${#commands[@]}" -ne "${#expected_output[@]}" ]; then
+    echo "Erreur : Les tableaux 'commands' et 'expected_output' doivent avoir la même longueur."
+    exit 1
+fi
+
+echo
+echo
+echo -e "                ${GREEN}TESTS MINISHELL (WIP)${NC}"
+echo
+echo
+
+error_count=0
+
+# Gestion des arguments en ligne de commande
+if [ "$#" -eq 0 ]; then
+    # Si aucun argument n'est specifie, execute tous les tests
+    for ((i = 0; i < ${#commands[@]}; i++)); do
+        cmd="${commands[i]}"
+        expected="${expected_output[i]}"
+        j=$((i + 1))
+
+        # Execution de la commande et stockage de la sortie dans une variable
+        output=$(./minishell <<< "$cmd")
+
+        # Comparaison de la sortie reelle avec la sortie attendue
+        if [[ "$output" == *"$expected"* ]]; then
+            echo -e "${MAGENTA}Commande n*$j${NC}: '$cmd' - ${GREEN}Test reussi${NC}"
+        else
+            echo -e "${MAGENTA}Commande n*$j${NC}: '$cmd' - ${RED}Test echoue${NC}"
+           # echo "Sortie reelle : '$output'"
+           # echo "Sortie attendue : '$expected'"
+			error_count=$((error_count + 1))
+        fi
+        echo
+    done
+
+	# Affiche le nombre d'erreurs detectees
+	echo
+	if [ "$error_count" -eq 0 ]; then
+		echo -e "${GREEN}Aucune erreur detectee, congrats!${NC}"
+	else
+		echo -e "${RED}Nombre d'erreurs detectees : $error_count"
+		echo -e "Relancer le script en specifiant le n* de la commande pour plus de details${NC}"
+	fi
+	echo
+elif [ "$#" -eq 1 ]; then
+    # Si un argument est specifie, execute le test correspondant
+    test_number="$1"
+    if [ "$test_number" -ge 1 ] && [ "$test_number" -le "${#commands[@]}" ]; then
+        i=$((test_number - 1))
+        cmd="${commands[i]}"
+        expected="${expected_output[i]}"
+        j=$((i + 1))
+
+        # Execution de la commande et stockage de la sortie dans une variable
+        output=$(./minishell <<< "$cmd")
+
+        # Comparaison de la sortie reelle avec la sortie attendue
+		echo
+        if [[ "$output" == *"$expected"* ]]; then
+            echo -e "${MAGENTA}Commande n*$j${NC}: '$cmd' - ${GREEN}Test reussi${NC}"
+            echo
+			echo -e "${GREEN}Sortie reelle${NC} : '$output'"
+			echo
+            echo -e "${GREEN}Sortie attendue${NC} : '$expected'"
+        else
+            echo -e "${MAGENTA}Commande n*$j${NC}: '$cmd' - ${RED}Test echoue${NC}"
+            echo
+			echo -e "${RED}Sortie reelle${NC} : '$output'"
+			echo
+            echo -e "${GREEN}Sortie attendue${NC} : '$expected'"
+        fi
+    else
+        echo "Numero de test invalide. Specifier un numero de test entre 1 et ${#commands[@]}."
+    fi
+else
+    echo "Utilisation : $0 [numero-du-test]"
+    echo "Si aucun argument n'est specifie, tous les tests seront executes. Sinon, seul le test specifie sera execute."
+fi
