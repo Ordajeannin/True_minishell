a7a8102ffbbd9305348db72440acfa2d90411b70
Antoine JEANNIN <ajeannin@student.42.fr>
2023-10-03 13:48:55 +0200

    FIX start with | issue
    Un input commencant par un pipe est maintenant invalide
    str | | ne devrait pas renvoyer d'invite de commande, mais au moins ne
    segfault plus et renvoie une erreur
    attention a ces erreurs :
    . << EOF consecutifs
    . | + ctrlD
    . ctrlC doit quitter cat << EOF
    
    Norme KO (!!!!)
    Compilation OK

--- srcs/redirection/erreur.c
+++ srcs/redirection/erreur.c
@@ -6,7 +6,7 @@
 /*   By: ajeannin <ajeannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 19:58:53 by ajeannin          #+#    #+#             */
-/*   Updated: 2023/10/02 16:57:28 by ajeannin         ###   ########.fr       */
+/*   Updated: 2023/10/03 13:37:55 by ajeannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 /*
  * Verifie si l'argument suivant une redirection est au bon format
- * (premier caractere non-alphanum)
+ * (premier caractere non-alphanum + absence de pipe en debut d'input)
 */
 int	is_correct_format(t_args **list)
 {
 	t_args	*current;
 
 	current = *list;
+	if (current->token == 4)
+	{
+		perror("syntax error near unexpected token\n");
+		return (-1);
+	}
 	while (current != NULL)
 	{
 		if (current->token == 4 || current->token == 5 || current->token == 6
